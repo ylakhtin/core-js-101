aaf7f64cf372f9b032a635c978d86a26fe338830
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-06-05 05:09:18 +0200

    feat: add Date task solutions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,21 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = 1900 + date.getYear();
+  let res = false;
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      // step 3
+      if (year % 400 === 0) {
+        res = true;
+      }
+    } else {
+      // step 4
+      res = true;
+    }
+  }
+  return res;
 }
 
 
@@ -73,8 +86,9 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = endDate - startDate;
+  return new Date(diff).toISOString().slice(11, 23);
 }
 
 
@@ -94,8 +108,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hour = (date.getUTCHours() % 12) * 30;
+  const minute = date.getUTCMinutes() * 6;
+  let angle = Math.abs(hour + (date.getUTCMinutes() / 2) - minute) % 360;
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return ((angle / 180) * Math.PI);
 }
 
 
